bd7d8d6178d9193aeb78336e6c275c18ba546d43
Noah Young <n.young@student.isartdigital.com>
2014-03-21 13:02:45 +0100

    PFA PROJECT
    *All files

---
+++ Assets/PFA_Assets/Scripts/PFA_TPCamera.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PFA_TPCamera : MonoBehaviour 
+{
+	// Camera
+	public Transform cameraTransform;
+	public Transform cameraTarget;
+	public float rotSpeed = 5f;
+	public float camDistance = 2.435931f;
+	public float camZDistance = 2.068446f;
+	public float camXRot = 26.06088f;
+	
+	// Controls
+	Vector2 camStickInput;
+	float deadzone = 0.15f;	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		CheckCamInputs();
+		CameraPositionUpdate();
+		CameraMovement();
+	}
+	
+	void CameraPositionUpdate()
+	{
+		float playerRot = transform.rotation.y;
+		float newX = transform.position.x - camZDistance * Mathf.Cos (playerRot);
+		float newY = cameraTransform.position.y;
+		float newZ = camZDistance * Mathf.Sin(playerRot);
+		
+		cameraTransform.position = new Vector3(newX, newY, newZ);
+		
+		//cameraTransform.position = new Vector3(transform.position.x + offsetX, cameraTransform.position.y, transform.position.z + offsetZ);
+		cameraTransform.LookAt(cameraTarget.position);
+	}
+	
+	void CheckCamInputs()
+	{
+		// Movement Inputs
+		camStickInput = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
+		
+		if(camStickInput.magnitude < deadzone)
+		{
+			camStickInput = Vector2.zero;
+		}
+		
+		Debug.Log ("Hcam = " + camStickInput.x);
+		Debug.Log ("Vcam = " + camStickInput.y);
+	}
+	
+	void CameraMovement()
+	{
+		this.transform.Rotate(camStickInput.x * this.transform.up * rotSpeed);
+		//cameraTransform.RotateAround(this.transform.position, this.transform.up, rotSpeed * camStickInput.x);
+	}
+}
---
+++ Assets/PFA_Assets/Scripts/PFA_TPControl.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PFA_TPControl : MonoBehaviour 
+{
+	// Public informations
+	public float _speed = 0.2f; // Vitesse
+	public float _runningSpeed = 0.4f; // Vitesse en sprint
+	public float _sidewaySpeedDivider = 0.75f;
+	public float _reversinSpeedDivider = 0.5f;
+	public float _rotSpeed = 3.5f;
+	public float _sidewayRotSpeed = 4.5f;
+	public float _reversinRotSpeed = 6f;
+	
+	// Movement vars
+	Vector2 stickInput;
+	float deadzone = 0.15f;
+	
+	// Game states
+	bool _canJump = true;
+	bool _sprinting = false;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		// Lol.
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		CheckInputs();
+		MoveCharacter();
+	}
+	
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.transform.tag == "Ground")
+		{
+			Debug.Log ("CAN JUMP !");
+			_canJump = true;
+		}
+	}
+	
+	void CheckInputs()
+	{
+		// Movement Inputs		
+		stickInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		modifyInputs();
+		
+		//Debug.Log ("H = " + stickInput.x);
+		//Debug.Log ("V = " + stickInput.y);
+		
+		// Control inputs
+		// Jump inputs
+		if (Input.GetKeyDown(KeyCode.JoystickButton0) && _canJump)
+		{
+			Debug.Log("Jump");
+			_canJump = false;
+			
+			// ajouter linéairement pour etre compensé avec la gravité
+			this.transform.Translate(this.transform.up);
+		}
+		// Sprint input
+		if (Input.GetKeyDown(KeyCode.JoystickButton2))
+		{
+			Debug.Log("Sprint");
+			_sprinting = true;			
+		}
+		
+		if (Input.GetKeyUp (KeyCode.JoystickButton2))
+		{
+			Debug.Log ("Stopped Sprinting");
+			_sprinting = false;
+		}
+		
+		// Shout input
+		if (Input.GetKeyDown(KeyCode.JoystickButton3))
+		{
+			Debug.Log("Shout");	
+		}	
+		
+		// Roll-up input
+		if (Input.GetKey(KeyCode.JoystickButton1))
+		{
+			Debug.Log("Roll-up");
+		}
+	}
+	
+	void modifyInputs()
+	{
+		if(stickInput.magnitude < deadzone)
+		{
+			stickInput = Vector2.zero;
+		}
+		
+		if(stickInput.y > deadzone)
+		{
+			stickInput.y = 1;
+		}
+		else if (stickInput.y < -deadzone)
+		{
+			stickInput.y = -1; // gérer ca
+		}
+		else
+		{
+			stickInput.y = 0;
+		}
+	}
+	
+	void MoveCharacter()
+	{
+		// Is sprinting ? Check speed
+		float currentSpeed = 0f;
+		
+		if(!_sprinting)
+		{
+			currentSpeed = _speed;
+		}
+		else if (_sprinting)
+		{
+			currentSpeed = _runningSpeed;
+		}
+		
+		
+		//Rotate
+		if(stickInput.y == -1)
+		{
+			if(stickInput.x != 0)
+			{
+				this.transform.Rotate(stickInput.y * -(stickInput.x/(Mathf.Abs(stickInput.x))) * this.transform.up * _reversinRotSpeed);
+			}
+			else
+			{
+				this.transform.Rotate(stickInput.y * this.transform.up * _reversinRotSpeed);
+			}
+		}
+		else if(stickInput.y == 0)
+		{
+			this.transform.Rotate(stickInput.x * this.transform.up * _rotSpeed);
+		}
+		else
+		{
+			this.transform.Rotate(stickInput.x * this.transform.up * _rotSpeed);
+		}
+	
+		// Movement
+		if(stickInput.y == -1)
+		{
+			this.transform.Translate(0, 0, Mathf.Abs(stickInput.y) * currentSpeed/_reversinSpeedDivider);
+		}
+		else if(stickInput.y == 0)
+		{
+			this.transform.Translate(0, 0, Mathf.Abs(stickInput.x) * currentSpeed/_sidewaySpeedDivider);
+		}
+		else
+		{
+			this.transform.Translate(0, 0, stickInput.y * currentSpeed);
+		}
+	}
+}
