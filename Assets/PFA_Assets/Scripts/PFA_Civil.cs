51a0f3951632c881be9f4b36c2a0d891c294816c
Noah Young <n.young@student.isartdigital.com>
2014-03-27 00:10:42 +0100

    CIVIL - ONGOING
    *Added model and color
    *Created civil prefab
    *Prepped civil func
    *Random mov tests

---
+++ Assets/PFA_Assets/Scripts/PFA_Civil.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PFA_Civil : MonoBehaviour 
+{
+	public float _speed = 4f;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		CivilMovement();
+	}
+	
+	void CivilMovement()
+	{
+		this.transform.Rotate(this.transform.up, Random.Range(-5f, 15f));
+		this.transform.Translate(Vector3.Normalize(this.transform.forward) * Time.deltaTime * _speed);
+		
+		this.transform.rotation = Quaternion.LookRotation(Vector3.Normalize(this.transform.forward) * Time.deltaTime * _speed);
+	}
+}
