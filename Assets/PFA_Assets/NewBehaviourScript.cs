1519da06ff533eb4994e79ca89ffdd45fdf86e15
CalvinCharbit <calvin.charbit@gmail.com>
2014-03-28 11:48:30 +0100

    ZoneExplosion - Nouveau Rendu

--- Assets/PFA_Assets/NewBehaviourScript.cs
+++ Assets/PFA_Assets/NewBehaviourScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
